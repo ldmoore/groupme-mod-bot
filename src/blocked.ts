caa155c850736b8571d48616d91383c2479ed952
ldmoore <lucasdmo@unc.edu>
2025-08-25 10:22:25 -0400

    Fix blocked term list duplicate

--- src/blocked.ts
+++ src/blocked.ts
@@ -1,7 +1,7 @@
 const blocked = [
     "giving away billie eilish",
     "giving away sabrina carpenter",
-    "giving away sabrina carpenter",
+    "giving away tate mcrae",
     "macbook air"
 ];
 
