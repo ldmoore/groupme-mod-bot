8586ba5b17004fea60a5068abf1eb36a8ff60c78
ldmoore <lucasdmo@unc.edu>
2025-08-25 17:18:38 -0400

    Add season ticket to filter

--- src/blocked.ts
+++ src/blocked.ts
@@ -2,7 +2,8 @@ const blocked = [
     "giving away billie eilish",
     "giving away sabrina carpenter",
     "giving away tate mcrae",
-    "macbook air"
+    "macbook air",
+    "season ticket"
 ];
 
 export default blocked;
