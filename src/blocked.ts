cae8c407e4cd74d32feab737cf817868035224a3
ldmoore <lucasdmo@unc.edu>
2025-09-10 12:20:44 -0400

    Update filter

--- src/blocked.ts
+++ src/blocked.ts
@@ -2,10 +2,24 @@ const blocked = [
     "giving away billie eilish",
     "giving away sabrina carpenter",
     "giving away tate mcrae",
+    "selling tate mcrae",
+    "my tate mcrae",
+    "tate mcrae tickets",
+    "sabrina carpenter tickets",
+    "billie eilish tickets",
     "macbook air",
     "season ticket",
     "season football",
-    "macbook pro"
+    "macbook pro",
+    "sell my season",
+    "sell billie eilish",
+    "sell sabrina carpenter",
+    "sell tate mcrae",
+    "sell my ps5",
+    "give away ps5",
+    "ps5 for free",
+    "ps5 out for free",
+    "text me on gmail"
 ];
 
 export default blocked;
