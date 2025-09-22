4469c8e8290d1438d3681bda06f3da4295d85966
Nathan Santiago <38706493+nathansantiago@users.noreply.github.com>
2025-09-22 17:16:22 -0400

    Add 1 in 1 million chance of sending bots begone message (#5)
    * Add 1 in 1 million chance of sending bots begone message
    
    * Fix indentation of webhook; change random number to be more random

--- src/index.ts
+++ src/index.ts
@@ -62,17 +62,19 @@ app.post("/webhook", async (c) => {
       "Remove User"
     );
 
-	await safeFetch(
-		`https://api.groupme.com/v3/groups/${group_id}/members/${membership_id}/remove?token=${token}`,
-		{ method: "POST" },
-		"Remove User"
-	);
+    await safeFetch(
+      `https://api.groupme.com/v3/groups/${group_id}/members/${membership_id}/remove?token=${token}`,
+      { method: "POST" },
+      "Remove User"
+    );
 
-	/*await safeFetch(
-		`https://api.groupme.com/v3/bots/post`,
-		{ method: "POST", body: JSON.stringify( { bot_id, text: "BOTS BEGONE 🤬" }) },
-		"Post Bot Message"
-	);*/
+    if (Math.floor(Math.random() * 1000000) === 462926) {
+      await safeFetch(
+        `https://api.groupme.com/v3/bots/post`,
+        { method: "POST", body: JSON.stringify( { bot_id, text: "BOTS BEGONE 🤬" }) },
+        "Post Bot Message"
+      );
+    }
   } else {
     console.log("Message passed moderation check.");
   }
