8554cfa18e1db874aca85d4c3dbcbd53e78b43bc
Sean Blizard <emailseanblizard@gmail.com>
2025-10-29 18:02:31 -0400

    [test] webhook handler test suite'

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,14 @@ module.exports = {
 	roots: ["<rootDir>/test"],
 	testMatch: ["**/*.test.ts"],
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts", "!src/types.ts"],
+	coverageThreshold: {
+		global: {
+			branches: 80,
+			functions: 80,
+			lines: 80,
+			statements: 80,
+		},
+	},
+	coverageReporters: ["text", "lcov", "html"],
 };
--- test/blocked-phrases.test.ts
+++ test/blocked-phrases.test.ts
@@ -1,43 +1,51 @@
 import containsBlockedPhrase from "../src/blocked-phrases";
 
-test("click the link below", () => {
-	expect(containsBlockedPhrase("Please click the link below to verify")).toBe(
-		true,
-	);
-});
+describe(containsBlockedPhrase, () => {
+	test("click the link below", () => {
+		expect(containsBlockedPhrase("Please click the link below to verify")).toBe(
+			true,
+		);
+	});
 
-test("contact me on whatsapp", () => {
-	expect(
-		containsBlockedPhrase("You can contact me on WhatsApp for more info"),
-	).toBe(true);
-});
+	test("contact me on whatsapp", () => {
+		expect(
+			containsBlockedPhrase("You can contact me on WhatsApp for more info"),
+		).toBe(true);
+	});
 
-test("free cash giveaway", () => {
-	expect(containsBlockedPhrase("Join our free cash giveaway now!")).toBe(true);
-});
+	test("free cash giveaway", () => {
+		expect(containsBlockedPhrase("Join our free cash giveaway now!")).toBe(
+			true,
+		);
+	});
 
-test("no blocked phrase", () => {
-	expect(containsBlockedPhrase("This message is clean and safe.")).toBe(false);
-});
+	test("no blocked phrase", () => {
+		expect(containsBlockedPhrase("This message is clean and safe.")).toBe(
+			false,
+		);
+	});
 
-test("you have been selected", () => {
-	expect(
-		containsBlockedPhrase(
-			"Congratulations! You have been selected for a prize.",
-		),
-	).toBe(true);
-});
+	test("you have been selected", () => {
+		expect(
+			containsBlockedPhrase(
+				"Congratulations! You have been selected for a prize.",
+			),
+		).toBe(true);
+	});
 
-test("verify your account", () => {
-	expect(containsBlockedPhrase("Please verify your account to continue.")).toBe(
-		true,
-	);
-});
+	test("verify your account", () => {
+		expect(
+			containsBlockedPhrase("Please verify your account to continue."),
+		).toBe(true);
+	});
 
-test("clean message", () => {
-	expect(containsBlockedPhrase("This message is safe and clean.")).toBe(false);
-});
+	test("clean message", () => {
+		expect(containsBlockedPhrase("This message is safe and clean.")).toBe(
+			false,
+		);
+	});
 
-test("empty message", () => {
-	expect(containsBlockedPhrase("")).toBe(false);
+	test("empty message", () => {
+		expect(containsBlockedPhrase("")).toBe(false);
+	});
 });
