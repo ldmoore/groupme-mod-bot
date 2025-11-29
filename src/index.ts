f0378abfd5ddbac084396e2d32ff7421bf83e052
Lucas Moore <lucasdmo@unc.edu>
2025-11-29 13:11:46 -0500

    Fix linting issues

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const app = new Hono<{ Bindings: Env }>();
 app.post("/webhook/:secret", async (c: Context) => {
 	const provided_secret = c.req.param("secret");
 
-	if (provided_secret !== c.env.WEBHOOK_SECRET){
-		return c.json({error: "Unauthorized"}, 401)
+	if (provided_secret !== c.env.WEBHOOK_SECRET) {
+		return c.json({ error: "Unauthorized" }, 401);
 	}
 	return groupMeWebhookHandler(c);
 });
